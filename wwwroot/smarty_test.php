c26089a798247d7adba51fdc535efaf6f63e39ab
umako <mssrumk96@gmail.com>
2017-05-23 18:34:25 +0900

    smarty test

---
+++ wwwroot/smarty_test.php
@@ -0,0 +1,29 @@
+<?php
+//  smarty_test.php
+
+//  smartyのインクルード
+require_once( __DIR__ . '/vendor/smarty-3.1.30/libs/Smarty.class.php');
+
+//  smartyの初期設定
+$smarty_obj = new Smarty();
+//var_dump($smarty_obj);
+$smarty_obj->setTemplateDir( __DIR__ . '/../smarty/templates/');
+$smarty_obj->setCompileDir( __DIR__ . '/../smarty/templates_c/');
+
+//  smartyへのデータの入力
+
+$s = 'データの入力テスト';
+$smarty_obj->assign('val', $s);
+$awk['a'] = '配列のa';
+$awk['b'] = '配列のb';
+$smarty_obj->assign('ar', $awk);
+
+//  テンプレートを指定して出力
+
+$smarty_obj->display('smarty_test.tpl');
+
+
+//  確認用
+//echo 'OK';
+
+?>
